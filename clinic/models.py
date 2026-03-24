21d1b63db44cceca226db0fad785c4699db5a705
Aleksandra Frąk <ola.weronika1@wp.pl>
2026-03-24 10:57:29 +0100

    Add Appointment model with extended status handling

--- clinic/models.py
+++ clinic/models.py
@@ -30,3 +30,26 @@ class Doctor(models.Model):
 
     def __str__(self):
         return f"Dr {self.first_name} {self.last_name} - {self.specialization}"
+
+class Appointment(models.Model):
+    STATUS_CHOICES = [
+        ("scheduled", "Scheduled"),
+        ("confirmed", "Confirmed"),
+        ("in_progress", "In Progress"),
+        ("completed", "Completed"),
+        ("cancelled", "Cancelled"),
+        ("no_show", "No show"),
+    ]
+
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="scheduled")
+    notes = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    upadated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.patient} - {self.doctor} on {self.date} at {self.time}"
