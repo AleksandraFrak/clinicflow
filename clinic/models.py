058818ffa4ab3004615897e50a53678b05466389
Aleksandra Frąk <ola.weronika1@wp.pl>
2026-03-24 10:30:54 +0100

    Add Patient model

--- clinic/models.py
+++ clinic/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    pesel = models.CharField(max_length=11, unique=True)
+    birth_date = models.DateField()
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
