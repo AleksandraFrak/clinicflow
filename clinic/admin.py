ef7f76eecff825685f2a84371ba3b1e8801cefb0
Aleksandra Frąk <ola.weronika1@wp.pl>
2026-03-24 11:27:24 +0100

    Customize Django admin for patient doctor and appointment models

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import Patient, Doctor, Appointment
 
-admin.site.register(Patient)
-admin.site.register(Doctor)
-admin.site.register(Appointment)
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'pesel', 'email', 'phone', 'birth_date')
+    search_fields = ('first_name', 'last_name', 'pesel', 'email')
+    ordering = ('first_name', 'last_name')
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'specialization', 'email', 'phone', 'room_number')
+    search_fields = ('first_name', 'last_name', 'email', 'room_number')
+    list_filter = ('specialization',)
+    ordering = ('first_name', 'last_name')
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'doctor', 'date', 'time', 'status', 'created_at')
+    search_fields = ('patient__first_name', 'patient__last_name', 'doctor__first_name', 'doctor__last_name')
+    list_filter = ('status', 'date', 'doctor')
+    ordering = ('date', 'time')
